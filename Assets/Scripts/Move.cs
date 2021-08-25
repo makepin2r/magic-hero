5a96142f63ffaab82b022943681c8648ad44aeb0
hi-robot <makepin2r@gmail.com>
2021-08-25 23:34:56 +0900

    Enemy 오브젝트가 Player 자동으로 따라가는 ai 구현

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -7,10 +7,12 @@ public class Move : MonoBehaviour
 {
     NavMeshAgent agent;
     public float speed = 3.0f;
+    private Animator animator;
 
     private void Awake()
     {
         agent = this.gameObject.GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Start is called before the first frame update
@@ -24,22 +26,27 @@ public class Move : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W)) // 菊
         {
+            animator.SetBool("isWalk", true);
             this.transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed);
         }
-
-        if (Input.GetKey(KeyCode.A)) // 哭率
+        else if (Input.GetKey(KeyCode.A)) // 哭率
         {
+            animator.SetBool("isWalk", true);
             this.transform.Translate(new Vector3(-1, 0, 0) * Time.deltaTime * speed);
         }
-        
-        if (Input.GetKey(KeyCode.S)) // 第
+        else if (Input.GetKey(KeyCode.S)) // 第
         {
+            animator.SetBool("isWalk", true);
             this.transform.Translate(new Vector3(0, 0, -1) * Time.deltaTime * speed);
         }
-        
-        if (Input.GetKey(KeyCode.D)) // 坷弗率
+        else if (Input.GetKey(KeyCode.D)) // 坷弗率
         {
+            animator.SetBool("isWalk", true);
             this.transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * speed);
+        }
+        else 
+        {
+            animator.SetBool("isWalk", false);
         }
     }
 }
