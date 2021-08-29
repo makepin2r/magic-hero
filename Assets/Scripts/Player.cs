e6d7769e0ac672217fdba807c49792a4312e1c59
hi-robot <makepin2r@gmail.com>
2021-08-29 23:26:39 +0900

    joystick 구현, 플레이어 이동 리팩토링

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,50 +5,13 @@ using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
-    // Input Axis값을 받을 전역변수
-    float hAxis;
-    float vAxis;
-    Vector3 moveVec;
-
-    public float speed = 3.0f;
-    
-    NavMeshAgent agent;
-    private Animator animator;
-    Rigidbody rigid;
-
     private void Awake()
     {
-        agent = this.gameObject.GetComponent<NavMeshAgent>();
-        animator = GetComponentInChildren<Animator>();
-        rigid = GetComponent<Rigidbody>();
-    }
-
-    void GetInput()
-    {
-        hAxis = Input.GetAxisRaw("Horizontal");
-        vAxis = Input.GetAxisRaw("Vertical");
-    }
-
-    void Move()
-    {
-        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
-
-        transform.position += moveVec * speed * Time.deltaTime;
-
-        animator.SetBool("isWalk", moveVec != Vector3.zero);
-    }
-
-    void Turn()
-    {
-        transform.LookAt(transform.position + moveVec); // 나아가는 방향으로 회전 
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetInput();
-        Move();
-        Turn();
     }
 
 }
